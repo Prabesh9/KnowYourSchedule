dbf3aa3741f1f441da6ab69ba33f38dc2c2ff89c
Aadarsha Maharjan <aadarshamaharjan@gmail.com>
2019-07-28 18:58:52 +0545

    Tryy commit

--- forum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('new/', views.index, name='newforum'),
-    path('delete/', views.delete_forum, name='deleteforum'),
-]
